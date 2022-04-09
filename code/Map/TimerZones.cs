b6a64f06b1cd50d2e3a4f6d04cc300c825d82be2
Jake Evans <jacob.ee92@gmail.com>
2022-04-08 19:56:05 -0600

    Basic zones

---
+++ code/Map/TimerZones.cs
@@ -0,0 +1,39 @@
+﻿
+using Sandbox;
+
+namespace Surf.Map;
+
+internal partial class BaseZone : BaseTrigger 
+{ 
+
+}
+
+[Library( "surf_timer_start", Description = "Where the timer will start" )]
+internal partial class StartZone : BaseZone
+{
+
+}
+
+[Library( "surf_timer_end", Description = "Where the timer will end" )]
+internal partial class EndZone : BaseZone
+{
+
+}
+
+[Library( "surf_timer_stage_start", Description = "Where the timer will begin a stage" )]
+internal partial class StageStartZone : BaseZone 
+{
+
+}
+
+[Library( "surf_timer_stage_end", Description = "Where the timer will end a stage" )]
+internal partial class StageEndZone : BaseZone
+{
+
+}
+
+[Library( "surf_timer_checkpoint", Description = "Where the timer will set a checkpoint (linear)" )]
+internal partial class CheckpointZone : BaseZone
+{
+
+}
