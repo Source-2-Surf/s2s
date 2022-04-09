4722cd30e8925399e2e55beacfa4afd414352b77
Jake Evans <jacob.ee92@gmail.com>
2022-04-08 21:21:58 -0600

    Shifting things around

--- code/Map/TimerZones.cs
+++ code/Map/TimerZones.cs
@@ -1,39 +1,41 @@
 ﻿
+using Hammer;
 using Sandbox;
 
 namespace Surf.Map;
 
-internal partial class BaseZone : BaseTrigger 
+public partial class BaseZone : BaseTrigger 
 { 
 
 }
 
-[Library( "surf_timer_start", Description = "Where the timer will start" )]
-internal partial class StartZone : BaseZone
+[EntityTool("test", "test2")]
+[Library( "surf_linear_start", Description = "Where the timer will start" )]
+public partial class LinearStart : BaseZone
 {
 
 }
 
-[Library( "surf_timer_end", Description = "Where the timer will end" )]
-internal partial class EndZone : BaseZone
+[Library( "surf_linear_end", Description = "Where the timer will end" )]
+internal partial class LinearEnd : BaseZone
 {
 
 }
 
-[Library( "surf_timer_stage_start", Description = "Where the timer will begin a stage" )]
-internal partial class StageStartZone : BaseZone 
+[Library( "surf_linear_checkpoint", Description = "Where the timer will set a checkpoint (linear)" )]
+internal partial class LinearCheckpoint : BaseZone
 {
 
 }
 
-[Library( "surf_timer_stage_end", Description = "Where the timer will end a stage" )]
-internal partial class StageEndZone : BaseZone
+[Library( "surf_stage_start", Description = "Where the timer will begin a stage" )]
+internal partial class StageStart : BaseZone 
 {
 
 }
 
-[Library( "surf_timer_checkpoint", Description = "Where the timer will set a checkpoint (linear)" )]
-internal partial class CheckpointZone : BaseZone
+[Library( "surf_stage_end", Description = "Where the timer will end a stage" )]
+internal partial class StageEnd : BaseZone
 {
 
 }
